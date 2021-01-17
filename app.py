92698c1f786057ad5a6cfce495c2dde62b69757c
googoodoll25 <pt.tharn@gmail.com>
2021-01-17 20:48:49 +0700

    Install venv

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('GIT')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello WIM'
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
