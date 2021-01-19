c2f2b80ad7e3ee2faf7d2f5161e3577f9f3de203
googoodoll25 <pt.tharn@gmail.com>
2021-01-19 22:35:37 +0700

    ChartJS

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('Realtime.html')
+    now = datetime.now()
+    date = now.strftime('%d/%m/%Y')
+    return render_template('Realtime.html', date=date)
+
+
+@ app.route('/report')
+def report():
+    return 'Report'
 
 
 if __name__ == "__main__":
